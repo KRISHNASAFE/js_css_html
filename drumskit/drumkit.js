9d8b01900022f0512300daa2aea30294f7be5d22
KRISHNASAFE <163742545+KRISHNASAFE@users.noreply.github.com>
2025-03-22 11:54:47 +0530

    Add files via upload

---
+++ drumskit/drumkit.js
@@ -0,0 +1,36 @@
+const kits = ["crash", "kick", "snare", "tom"];
+
+const containerEl = document.querySelector(".container");
+
+kits.forEach(kit=>{
+    const btnEl = document.createElement("button");
+    btnEl.classList.add("btn");
+    btnEl.innerText = kit
+    btnEl.style.backgroundImage = "url(images/" + kit + ".png)";
+    containerEl.appendChild(btnEl);
+    const audioEl = document.createElement("audio");
+    audioEl.src = "sounds/" + kit + ".mp3";
+    containerEl.appendChild(audioEl);
+    btnEl.addEventListener("click", ()=>{
+        audioEl.play();
+    });
+    window.addEventListener("keydown", (event)=>{
+        if(event.key === kit.slice(0, 1)){
+            audioEl.play();
+            btnEl.style.transform = "scale(.8)";
+            setTimeout(()=>{
+                btnEl.style.transform = "scale(1)";
+            }, 500);
+        };
+    });
+});
+
+
+
+
+/*
+    <button class="btn">Snare</button>
+    <button class="btn">Kick</button>
+    <button class="btn">Crash</button>
+    <button class="btn">Tom</button>
+*/
